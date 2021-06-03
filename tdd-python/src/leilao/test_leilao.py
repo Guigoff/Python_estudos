96fb4b9b79a6906e046ead7a6eb6ee7ab9c17298
Guilherme Furlanetto <gui.furlanetto@hotmail.com>
2021-06-03 14:34:19 -0300

    Atualizações das aulas

---
+++ tdd-python/src/leilao/test_leilao.py
@@ -0,0 +1,65 @@
+from unittest import TestCase
+from src.leilao.dominio import Usuario, Lance, Leilao
+
+
+class TestLeilao(TestCase):
+
+    def setUp(self):
+        self.user_1 = Usuario('Fi')
+        self.lance_1 = Lance(self.user_1, 100.0)
+        self.leilao = Leilao('Carro')
+
+    def test_deve_retornar_o_maior_e_o_menor_valor_de_um_lance_quando_adicionados_em_ordem_crescente(self):
+
+        user_2 = Usuario('Fu')
+        lance_2 = Lance(user_2, 200.0)
+
+        self.leilao.propoe(self.lance_1)
+        self.leilao.propoe(lance_2)
+
+        menor_valor_esperado = 100
+        maior_valor_esperado = 200
+
+        self.assertEqual(menor_valor_esperado, self.leilao.menor_lance)
+        self.assertEqual(maior_valor_esperado, self.leilao.maior_lance)
+
+    def test_deve_retornar_o_maior_e_o_menor_valor_de_um_lance_quando_adicionados_em_ordem_decrescente(self):
+
+        user_2 = Usuario('Fu')
+        lance_2 = Lance(user_2, 200.0)
+
+        self.leilao.propoe(lance_2)
+        self.leilao.propoe(self.lance_1)
+
+        menor_valor_esperado = 100
+        maior_valor_esperado = 200
+
+        self.assertEqual(menor_valor_esperado, self.leilao.menor_lance)
+        self.assertEqual(maior_valor_esperado, self.leilao.maior_lance)
+
+    def test_deve_retornar_o_mesmo_valor_para_maior_e_menor_lance_quando_leilao_tiver_apenas_um_lance(self):
+        self.leilao.propoe(self.lance_1)
+
+        menor_valor_esperado = 100
+        maior_valor_esperado = 100
+
+        self.assertEqual(menor_valor_esperado, self.leilao.menor_lance)
+        self.assertEqual(maior_valor_esperado, self.leilao.maior_lance)
+
+    def test_deve_retornar_o_maior_e_o_menor_valor_de_um_lance_quando_o_leilao_tiver_tres_lances(self):
+        user_2 = Usuario('Fu')
+        user_3 = Usuario('Fa')
+        lance_2 = Lance(user_2, 200.0)
+        lance_3 = Lance(user_3, 500.0)
+
+        self.leilao.propoe(self.lance_1)
+        self.leilao.propoe(lance_2)
+        self.leilao.propoe(lance_3)
+
+
+
+        menor_valor_esperado = 100
+        maior_valor_esperado = 500
+
+        self.assertEqual(menor_valor_esperado, self.leilao.menor_lance)
+        self.assertEqual(maior_valor_esperado, self.leilao.maior_lance)
